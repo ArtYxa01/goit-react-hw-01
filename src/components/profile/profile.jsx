8adc9591846d7eb639a5811ebe69432802b46c3e
ArtYxa01 <115730030+ArtYxa01@users.noreply.github.com>
2023-05-21 22:37:36 +0300

    fctyvgunjk

---
+++ src/components/profile/profile.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import sty from './profile.module.css'
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  // const ( followers, views, likes ] = stats;
+  return (
+  <div className={sty.profile}>
+  <div className={sty.description}>
+  <img src={avatar} alt="User avatar" className= {sty.avatar}/>
+   <p className= {sty.name}>{username}</p>
+    <p className={sty.tag}>@{tag}</p>
+    <p className= {sty.location}>{location}</p>
+  </div>
+  <ul className={sty.stats}>
+  <li>
+  <span className={sty.label}>Followers</span>
+  <span className={sty.quantity}>{stats.followers}</span>
+  </li>
+   <li>
+  <span className={sty.label}>Views</span>
+   <span className={sty.quantity}>{stats.views}</span>
+  </li>
+  <li>
+  <span className={sty.label}>Likes</span>
+   <span className={sty.quantity}>{stats.likes}</span>
+  </li> 
+  </ul>
+  </div>
+  )
+  }
+  Profile.proptypes = {
+    username: Proptypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isrequired,
+    stats: PropTypes. shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+}
